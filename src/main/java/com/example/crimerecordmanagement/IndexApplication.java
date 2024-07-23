e197cc685faaa321574f19b927988fafe4c2334e
Your Name <muhdsanee87@gmail.com>
2024-07-23 10:42:41 +0100

    refactor

--- src/main/java/com/example/crimerecordmanagement/IndexApplication.java
+++ src/main/java/com/example/crimerecordmanagement/IndexApplication.java
@@ -1,11 +1,11 @@
-package com.example.crime;
+package com.example.crimerecordmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("com.example.demo")
+@EntityScan("com.example.crimerecordmanagement")
 public class IndexApplication {
 
 	public static void main(String[] args) {
